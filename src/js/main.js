1dad60cbd1e5dca96282e55ad21b5b9d67ed2636
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-01-31 00:24:03 +0100

    made hamburgermenu

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,9 @@
 import "./../scss/style.scss";
+
+const hamburgerSymbol = document.getElementById("hamburgerSymbol");
+const nav = document.querySelector("nav");
+
+hamburgerSymbol.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  hamburgerSymbol.classList.toggle("active");
+});
