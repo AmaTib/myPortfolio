d451251ed059094e3dc31f4e702fa3a54b6aefc5
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-01-13 17:30:38 +0100

    Add font and style homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,19 @@
+import "../styles/Homepage.css";
+
 export const HomePage = () => {
-  return <>Hem</>;
+  return (
+    <>
+      <section className="mainContent">
+        <div className="nameContainer">
+          <h1>Amanda</h1>
+        </div>
+        <div className="underLine"></div>
+        <div className="textContainer">
+          <p>Front</p>
+          <p>End</p>
+          <p>Developer</p>
+        </div>
+      </section>
+    </>
+  );
 };
