17e359f4669b4eec3217e08046327068616c6283
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-01-21 16:28:32 +0100

    Add screen media query for homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,18 @@
+import { useState, useEffect } from "react";
 import "../styles/Homepage.css";
 
 export const HomePage = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+
+  //this useeffect makes sure the width state updates when resizing the window
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(width);
+
   return (
     <>
       <section className="mainContent">
@@ -9,10 +21,15 @@ export const HomePage = () => {
         </div>
         <div className="underLine"></div>
         <div className="textContainer">
-          <p>Front</p>
-          <p>End</p>
-          <p>Developer</p>
-          <a href=""></a>
+          {width < 600 ? (
+            <>
+              <p>Front</p>
+              <p>End</p>
+              <p>Developer</p>
+            </>
+          ) : (
+            <p>Front End Developer</p>
+          )}
         </div>
       </section>
     </>
