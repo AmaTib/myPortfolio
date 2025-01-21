15780b50c4f3b425575d0e953fd98360f375abfc
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-01-21 17:13:19 +0100

    Fix and style footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,24 @@
+import "../styles/footer.css";
 import { FiGithub } from "react-icons/fi";
 import { FiLinkedin } from "react-icons/fi";
+import { RxOpenInNewWindow } from "react-icons/rx";
 
 export const Footer = () => {
   return (
     <>
       <footer>
-        <p>
-          <FiGithub /> Github
-        </p>
-        <p>
-          <FiLinkedin /> Linkedin
-        </p>
+        <a href="https://github.com/AmaTib" target="_blank">
+          <FiGithub className="icon" />
+          <span className="iconText">
+            Github <RxOpenInNewWindow />
+          </span>
+        </a>
+        <a href="https://www.linkedin.com/in/amanda-tibell/" target="_blank">
+          <FiLinkedin className="icon" />
+          <span className="iconText">
+            Linkedin <RxOpenInNewWindow />
+          </span>
+        </a>
       </footer>
     </>
   );
