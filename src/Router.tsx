f54ee792701dc7db32ec6c19095859e327d8319c
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2025-01-25 18:34:41 +0100

    Set up for deploy

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,17 +4,20 @@ import { HomePage } from "./pages/HomePage.tsx";
 import { ProjectsPage } from "./pages/ProjectsPage.tsx";
 import { AboutPage } from "./pages/About.tsx";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      { path: "/projects", element: <ProjectsPage /> },
-      { path: "/about", element: <AboutPage /> },
-    ],
-  },
-]);
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        { path: "/projects", element: <ProjectsPage /> },
+        { path: "/about", element: <AboutPage /> },
+      ],
+    },
+  ],
+  { basename: "/myPortfolio" }
+);
